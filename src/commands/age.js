71fb2998edce38c1fcb87c2b50232d0eaa6f6397
Jack Wright <1998jwright@gmail.com>
2021-01-17 21:12:36 +0000

    Greatly tidied up the age command

--- src/commands/age.js
+++ src/commands/age.js
@@ -1,28 +1,26 @@
+const getUser = require('./../functions/getUser');
+
 class age {
 	/**
-	 * This method will tell the user how old their or someone else's Discord account is as a reply in the channel they sent the message in.
+	 * This command will tell the user how old their or someone else's Discord account is as a reply in the channel they sent the message in.
 	 * @param {Message} msg
-	 * @param {String} cmd
-	 * @param {Array} args
+	 * @param {string} cmd
+	 * @param {array} args
 	 */
-	static execute = async (msg, args, cmd) => {
+	static execute = async (msg, args) => {
 		try {
-			let created;
-			let user;
-
-			if (!args.length) created = new Date(msg.author.createdTimestamp);
-			else {
-				const uid = args[0];
-				// For some reason, if the line below is not present, the user variable will turn out to be undefined. It might temporarily populate the cache?
-				await msg.guild.members.fetch(uid);
-				user = msg.guild.members.cache.get(uid).user;
-				created = new Date(user.createdTimestamp);
-			}
-
+			// Is the sender trying to find the age of themself (no args) or others (args[0] being user ID)?
+			const user = args.length ? await getUser(msg.guild, args[0]) : msg.author;
+			// Create a new Date instance with the UNIX timestamp
+			const created = new Date(user.createdTimestamp);
+			// Get today's time
 			const today = new Date();
+			// Calculate how many days the user has had Discord
 			const age = today.getTime() - created.getTime();
+			// Construct and reply with the user's age!
 			msg.reply(`\n**Account:** ${user ? user.username : msg.author.username}\n**Created on:** ${created.toDateString()}.\n**Age:** ${Math.round(age / 86400000)} days.`);
 		} catch (error) {
+			console.log(error);
 			msg.reply('For some reason I am unable to find the age of that user.');
 		}
 	};
---
+++ src/functions/getUser.js
@@ -0,0 +1,14 @@
+/**
+ * Resolve a GuildMember via the user's ID.
+ * @param {Guild} guild The guild to search in
+ * @param {string} userID The user's numerical ID
+ * @returns {Promise} GuildMember Promise
+ */
+async function getUser(guild, userID) {
+	// For some reason, if the line below is not present, the user variable has a high liklihood to turn out undefined. It might temporarily populate the cache?
+	await guild.members.fetch(userID);
+	const user = guild.members.cache.get(userID).user;
+	return Promise.resolve(user);
+}
+
+module.exports = getUser;
