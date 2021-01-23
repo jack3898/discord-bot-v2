137a52ca55db22ea93b8eee4a725f631401539d9
Jack Wright <1998jwright@gmail.com>
2021-01-23 16:16:50 +0000

    !age command can use usernames instead of user ids.
    To keep the code a little cleaner, I removed the feature that made it so that when you do not include a name, it would fetch the message author's date

--- src/commands/age.js
+++ src/commands/age.js
@@ -1,16 +1,17 @@
-const getUser = require('./../functions/getUser');
-
 class age {
 	/**
 	 * This command will tell the user how old their or someone else's Discord account is as a reply in the channel they sent the message in.
-	 * @param {Message} msg
-	 * @param {string} cmd
-	 * @param {array} args
+	 * @param {Message} msg Message instance, used to identify which channel to reply in
+	 * @param {array} args The name of the user. Will be joined to form a string
 	 */
 	static execute = async (msg, args) => {
 		try {
-			// Is the sender trying to find the age of themself (no args) or others (args[0] being user ID)?
-			const user = args.length ? await getUser(msg.guild, args[0]) : msg.author;
+			// Construct the query
+			const options = {query: args.join(' '), force: true};
+			// Wait for the script to fetch the user
+			const userCollection = await msg.guild.members.fetch(options);
+			// Extract the User instance from the query
+			const user = Array.from(userCollection)[0][1].user;
 			// Create a new Date instance with the UNIX timestamp
 			const created = new Date(user.createdTimestamp);
 			// Get today's time
@@ -20,8 +21,7 @@ class age {
 			// Construct and reply with the user's age!
 			msg.reply(`\n**Account:** ${user ? user.username : msg.author.username}\n**Created on:** ${created.toDateString()}.\n**Age:** ${Math.round(age / 86400000)} days.`);
 		} catch (error) {
-			console.log(error);
-			msg.reply('For some reason I am unable to find the age of that user.');
+			msg.reply('I am unable to find that user.');
 		}
 	};
 }
